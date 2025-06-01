6ceaac45badde0edaabd9959731e9552018a41be
Adrián Ucha Sousa <uchasousaa@gmail.com>
2025-06-01 17:01:41 +0200

    New Seeders Roles applied for User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
     /**
      * Attributes for show.
@@ -19,7 +20,6 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'role',
         'password_changed',
         'dni',
         'address',
