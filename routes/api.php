67e6b974c4f0f9ba4601f126cf1539adc6028627
Adrián Ucha Sousa <uchasousaa@gmail.com>
2025-06-08 14:01:18 +0200

    Added routes for Auth Controller

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Public Routes for all uses and access
+Route::prefix('auth')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/verify-2fa', [AuthController::class, 'verify2FA']);
+    Route::post('/request-password-reset', [AuthController::class, 'requestPasswordReset']);
+    Route::post('/register-admin', [AuthController::class, 'registerAdmin']);
+});
+
+// Protected routes with auth user all users
+Route::middleware(['auth:sanctum'])->group(function () {
+    // Password change
+    Route::post('/auth/change-password', [AuthController::class, 'changePassword']);
+});
